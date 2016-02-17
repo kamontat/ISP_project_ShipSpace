19225a239351f6f864fe075a11d8d023dfd253ed
kamontat <kamontat@MacBook-Pro.local>
2016-02-17 09:24:55 +0700

    Make ship can move in only up and right way :: Version 0.2.1

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,37 +1,36 @@
 var GameLayer = cc.LayerColor.extend({
+
+
     init: function () {
         console.log('Initialized');
         this._super(new cc.Color(127, 127, 127, 255));
         this.setPosition(new cc.Point(0, 0));
 
-        var ship = new Ship();
-        ship.setPosition(new cc.Point(200, 220));
-        this.addChild(ship);
-        console.log('Add ship to super class');
-        ship.scheduleUpdate();
+        // create Ship object
+        this.ship = new Ship();
+        this.ship.setPosition(new cc.Point(300, 220));
+        this.addChild(this.ship);
+        this.ship.scheduleUpdate();
 
         this.addKeyboardHandlers();
-        console.log("use keyboardhandlers");
 
         return true;
     },
+
     addKeyboardHandlers: function () {
         var self = this;
         cc.eventManager.addListener({
             event: cc.EventListener.KEYBOARD,
             onKeyPressed: function (keyCode, event) {
                 self.onKeyDown(keyCode, event);
-            },
-            onKeyReleased: function (keyCode, event) {
-                self.onKeyUp(keyCode, event);
             }
         }, this);
     },
+
     onKeyDown: function (keyCode, event) {
-        console.log('Down: ' + keyCode.toString());
-    },
-    onKeyUp: function (keyCode, event) {
-        console.log('Up: ' + keyCode.toString());
+        if (keyCode == cc.KEY.space) {
+            this.ship.switchDirection();
+        }
     }
 });
 
@@ -44,5 +43,3 @@ var StartScene = cc.Scene.extend({
         console.log('GameLayer created');
     }
 });
-
-
--- src/Ship.js
+++ src/Ship.js
@@ -2,17 +2,43 @@ var Ship = cc.Sprite.extend({
     ctor: function () {
         this._super();
         this.initWithFile("res/images/ship.png");
+        this.direction = Ship.DIR.UP;
     },
 
     update: function () {
         var pos = this.getPosition();
         this.setPosition(new cc.Point(pos.x, pos.y + 5));
 
-        if (pos.y < screenHeight) {
-            this.setPosition(new cc.Point(pos.x, pos.y + 5));
+        if (this.direction == 1) {
+            if (pos.y < screenHeight) {
+                this.setPosition(new cc.Point(pos.x, pos.y + 5));
+            } else {
+                this.setPosition(new cc.Point(pos.x, 0));
+            }
         } else {
-            this.setPosition(new cc.Point(pos.x, 0));
+            if (pos.x < screenWidth) {
+                this.setPosition(new cc.Point(pos.x + 5, pos.y));
+            } else {
+                this.setPosition(new cc.Point(0, pos.y));
+            }
+        }
+
+
+    },
+
+    switchDirection: function () {
+        console.log("Switch Finish");
+        if (this.direction == Ship.DIR.UP) {
+            this.direction = Ship.DIR.RIGHT;
+            this.setRotation(90);
+        } else {
+            this.direction = Ship.DIR.UP;
+            this.setRotation(0);
         }
     }
-
 });
+
+Ship.DIR = {
+    UP: 1,
+    RIGHT: 2
+}
