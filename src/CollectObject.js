546ad6585bd111cf0c8972fb629cb29f94435d4c
kamontat <kamontat_c@hotmail.com>
2016-04-26 10:39:41 +0700

    fixed some bug

--- src/CollectObject.js
+++ src/CollectObject.js
@@ -16,10 +16,10 @@ var CollectObject = cc.Sprite.extend({
         var myPos = this.getPosition();
         var oPos = obj.getPosition();
         return ((Math.abs(myPos.x - oPos.x) <= CollectObject.SIZE.X + 5) &&
-            (Math.abs(myPos.y - oPos.y) <= CollectObject.SIZE.Y + 5));
+        (Math.abs(myPos.y - oPos.y) <= CollectObject.SIZE.Y + 5));
     },
 
-    randomRange: function(first, second) {
+    randomRange: function (first, second) {
         var rand = Math.random() * second;
         while (rand < first) {
             rand = Math.random() * second;
